dd074f3899718e0cdf52bde20c2520efe5309a76
rishikarthikeyan2705 <129934783+rishikarthikeyan2705@users.noreply.github.com>
2023-04-05 11:05:07 +0400

    Updated the username

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # rishikarthikeyan2705
    # Input:
    # p, principal amount
    # t, time period in years
